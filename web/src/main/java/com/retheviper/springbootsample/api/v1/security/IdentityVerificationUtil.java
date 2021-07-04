23e18ce99c94999bcdf6285646da2b83343137cc
retheviper <retheviper@gmail.com>
2021-07-04 22:07:58 +0900

    fix

--- web/src/main/java/com/retheviper/springbootsample/api/v1/security/IdentityVerificationUtil.java
+++ web/src/main/java/com/retheviper/springbootsample/api/v1/security/IdentityVerificationUtil.java
@@ -1,19 +1,13 @@
 package com.retheviper.springbootsample.api.v1.security;
 
-import com.retheviper.springbootsample.application.service.member.UserDetailsServiceImpl;
 import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.Objects;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class IdentityVerificationUtil {
-
-    private final UserDetailsService service;
 
     public static boolean isLoginedUser(final String username) {
         return Objects.equals(username, SecurityContextHolder.getContext().getAuthentication().getName());
